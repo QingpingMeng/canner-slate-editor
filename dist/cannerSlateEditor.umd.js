da53550cba0894193886fb49f62ccd0c8597d86b
QingpingMeng <missing1989@gmail.com>
2018-08-31 22:42:26 -0700

    expose all plugin interfaces

--- dist/cannerSlateEditor.umd.js
+++ dist/cannerSlateEditor.umd.js
@@ -52830,7 +52830,7 @@ module.exports = exports['default'];
 
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.default = undefined;
 
@@ -52840,9 +52840,9 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _templateObject = _taggedTemplateLiteral(["\n  ", "\n  background-color: #FFF;\n  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 1px 1px rgba(0,0,0,0.16);\n  overflow-y: ", ";\n  overflow-x: hidden;\n"], ["\n  ", "\n  background-color: #FFF;\n  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 1px 1px rgba(0,0,0,0.16);\n  overflow-y: ", ";\n  overflow-x: hidden;\n"]),
-    _templateObject2 = _taggedTemplateLiteral(["\n  padding: 25px;\n  margin-top: ", ";\n"], ["\n  padding: 25px;\n  margin-top: ", ";\n"]),
-    _templateObject3 = _taggedTemplateLiteral(["\n  position: fixed;\n  top: 10px;\n  z-index: 10;\n  width: 100%;\n"], ["\n  position: fixed;\n  top: 10px;\n  z-index: 10;\n  width: 100%;\n"]);
+var _templateObject = _taggedTemplateLiteral(['\n    ', ' background-color: #FFF;\n    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 1px 1px rgba(0, 0, 0, 0.16);\n    overflow-y: ', ';\n    overflow-x: hidden;\n'], ['\n    ', ' background-color: #FFF;\n    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 1px 1px rgba(0, 0, 0, 0.16);\n    overflow-y: ', ';\n    overflow-x: hidden;\n']),
+    _templateObject2 = _taggedTemplateLiteral(['\n    padding: 25px;\n    margin-top: ', ';\n'], ['\n    padding: 25px;\n    margin-top: ', ';\n']),
+    _templateObject3 = _taggedTemplateLiteral(['\n    position: fixed;\n    top: 10px;\n    z-index: 10;\n    width: 100%;\n'], ['\n    position: fixed;\n    top: 10px;\n    z-index: 10;\n    width: 100%;\n']);
 
 var _react = __webpack_require__(0);
 
@@ -52968,172 +52968,184 @@ function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defi
 
 
 var plugins = [(0, _slateMdEditor.MarkdownPlugin)(), (0, _slatePrism2.default)({
-  onlyIn: function onlyIn(node) {
-    return node.type === 'code_block';
-  },
-  getSyntax: function getSyntax(node) {
-    return node.data.get('syntax');
-  }
+    onlyIn: function onlyIn(node) {
+        return node.type === 'code_block';
+    },
+    getSyntax: function getSyntax(node) {
+        return node.data.get('syntax');
+    }
 }), (0, _slateEditCode2.default)({
-  onlyIn: function onlyIn(node) {
-    return node.type === 'code_block';
-  }
+    onlyIn: function onlyIn(node) {
+        return node.type === 'code_block';
+    }
 }), (0, _slateTrailingBlock2.default)(), (0, _slateEditList2.default)(_slateHelperBlockList.DEFAULT), (0, _slateEditBlockquote2.default)(_slateHelperBlockQuote.DEFAULT), (0, _slateIconShared.ParagraphPlugin)(), (0, _slateIconBlockquote.BlockquotePlugin)(), (0, _slateIconBold.BoldPlugin)(), (0, _slateIconCode.CodePlugin)(), (0, _slateIconCodeblock.CodeBlockPlugin)(), (0, _slateIconFontbgcolor.FontBgColorPlugin)({
-  backgroundColor: function backgroundColor(mark) {
-    return mark.data.get('color').color;
-  }
+    backgroundColor: function backgroundColor(mark) {
+        return mark.data.get('color').color;
+    }
 }), (0, _slateIconFontcolor.FontColorPlugin)({
-  color: function color(mark) {
-    return mark.data.get('color').color;
-  }
+    color: function color(mark) {
+        return mark.data.get('color').color;
+    }
 }), (0, _slateIconItalic.ItalicPlugin)(), (0, _slateIconStrikethrough.StrikeThroughPlugin)(), (0, _slateIconUnderline.UnderlinePlugin)(), (0, _slateIconHeader.HeaderOnePlugin)(), (0, _slateIconHeader.HeaderTwoPlugin)(), (0, _slateIconHeader.HeaderThreePlugin)(), (0, _slateIconTable.TablePlugin)(), (0, _slateIconHr.HrPlugin)(), (0, _slateIconImage.ImagePlugin)(), (0, _slateIconLink.LinkPlugin)(), (0, _slateIconList.ListPlugin)(), (0, _slateIconVideo.VideoPlugin)()];
 
 var Container = _styledComponents2.default.div(_templateObject, function (props) {
-  return props.isFull && "\n    position: fixed;\n    top: 0;\n    left: 0;\n    bottom: 0;\n    right: 0;\n    z-index: 1000\n  ";
+    return props.isFull && '\n    position: fixed;\n    top: 0;\n    left: 0;\n    bottom: 0;\n    right: 0;\n    z-index: 1000\n  ';
 }, function (props) {
-  return props.isFull ? 'scroll' : 'hidden';
+    return props.isFull ? 'scroll' : 'hidden';
 });
 
 var EditorContainer = _styledComponents2.default.div(_templateObject2, function (props) {
-  return props.isFull ? '60px' : '10px';
+    return props.isFull ? '60px' : '10px';
 });
 
 var FixedToolbar = _styledComponents2.default.div(_templateObject3);
 
 var toolbarOptions = {
-  icons: [_slateIconBold2.default, _slateIconItalic2.default, _slateIconStrikethrough2.default, _slateIconUnderline2.default, _slateIconCode2.default, _slateIconClean2.default],
-  position: 'top',
-  disabledTypes: [_slateConstant.BLOCKS.CODE, _slateConstant.BLOCKS.CODE_LINE, _slateConstant.BLOCKS.HEADING_1, _slateConstant.BLOCKS.HEADING_2, _slateConstant.BLOCKS.HEADING_3, _slateConstant.BLOCKS.HEADING_4, _slateConstant.BLOCKS.HEADING_5, _slateConstant.BLOCKS.HEADING_6]
+    icons: [_slateIconBold2.default, _slateIconItalic2.default, _slateIconStrikethrough2.default, _slateIconUnderline2.default, _slateIconCode2.default, _slateIconClean2.default],
+    position: 'top',
+    disabledTypes: [_slateConstant.BLOCKS.CODE, _slateConstant.BLOCKS.CODE_LINE, _slateConstant.BLOCKS.HEADING_1, _slateConstant.BLOCKS.HEADING_2, _slateConstant.BLOCKS.HEADING_3, _slateConstant.BLOCKS.HEADING_4, _slateConstant.BLOCKS.HEADING_5, _slateConstant.BLOCKS.HEADING_6]
 };
 
 var sidebarOptions = {
-  icons: [{
-    icon: _slateIconList.OlList,
-    title: "Order List"
-  }, {
-    icon: _slateIconList.UlList,
-    title: "Unorder List"
-  }, {
-    icon: _slateIconHeader.Header1,
-    title: "Header One"
-  }, {
-    icon: _slateIconHeader.Header2,
-    title: "Header Two"
-  }, {
-    icon: _slateIconHr2.default,
-    title: "Ruler"
-  }, {
-    icon: _slateIconBlockquote2.default,
-    title: "Blockquote"
-  }]
+    icons: [{
+        icon: _slateIconList.OlList,
+        title: 'Order List'
+    }, {
+        icon: _slateIconList.UlList,
+        title: 'Unorder List'
+    }, {
+        icon: _slateIconHeader.Header1,
+        title: 'Header One'
+    }, {
+        icon: _slateIconHeader.Header2,
+        title: 'Header Two'
+    }, {
+        icon: _slateIconHr2.default,
+        title: 'Ruler'
+    }, {
+        icon: _slateIconBlockquote2.default,
+        title: 'Blockquote'
+    }]
 };
 
 var EditorComponent = function (_React$Component) {
-  _inherits(EditorComponent, _React$Component);
+    _inherits(EditorComponent, _React$Component);
 
-  function EditorComponent(props) {
-    _classCallCheck(this, EditorComponent);
+    function EditorComponent(props) {
+        _classCallCheck(this, EditorComponent);
 
-    var _this = _possibleConstructorReturn(this, (EditorComponent.__proto__ || Object.getPrototypeOf(EditorComponent)).call(this, props));
+        var _this = _possibleConstructorReturn(this, (EditorComponent.__proto__ || Object.getPrototypeOf(EditorComponent)).call(this, props));
 
-    _this.goFull = function () {
-      _this.setState({ isFull: !_this.state.isFull });
-    };
+        _this.goFull = function () {
+            _this.setState({ isFull: !_this.state.isFull });
+        };
 
-    _this.state = {
-      isFull: false
-    };
-    return _this;
-  }
-
-  _createClass(EditorComponent, [{
-    key: "render",
-    value: function render() {
-      var _props = this.props,
-          value = _props.value,
-          onChange = _props.onChange,
-          serviceConfig = _props.serviceConfig,
-          galleryConfig = _props.galleryConfig,
-          menuToolbarOption = _props.menuToolbarOption,
-          readOnly = _props.readOnly,
-          renderPlaceholder = _props.renderPlaceholder,
-          rest = _objectWithoutProperties(_props, ["value", "onChange", "serviceConfig", "galleryConfig", "menuToolbarOption", "readOnly", "renderPlaceholder"]);
-
-      var isFull = this.state.isFull;
-
-
-      return readOnly ? React.createElement(CannerEditor, { value: value, onChange: function onChange(arg) {
-          return arg;
-        }, readOnly: readOnly }) : React.createElement(
-        Container,
-        _extends({ isFull: isFull }, rest),
-        isFull ? React.createElement(
-          FixedToolbar,
-          null,
-          React.createElement(_menuToolbar2.default, {
-            isFull: true,
-            value: value,
-            serviceConfig: serviceConfig,
-            galleryConfig: galleryConfig,
-            menuToolbarOption: menuToolbarOption,
-            onChange: onChange,
-            goFull: this.goFull })
-        ) : React.createElement(_menuToolbar2.default, {
-          serviceConfig: serviceConfig,
-          galleryConfig: galleryConfig,
-          menuToolbarOption: menuToolbarOption,
-          value: value,
-          onChange: onChange,
-          goFull: this.goFull }),
-        React.createElement(
-          EditorContainer,
-          { isFull: isFull },
-          React.createElement(CannerEditor, { renderPlaceholder: renderPlaceholder, value: value, onChange: onChange, readOnly: readOnly })
-        )
-      );
+        _this.state = {
+            isFull: false
+        };
+        return _this;
     }
-  }]);
 
-  return EditorComponent;
+    _createClass(EditorComponent, [{
+        key: 'render',
+        value: function render() {
+            var _props = this.props,
+                value = _props.value,
+                onChange = _props.onChange,
+                serviceConfig = _props.serviceConfig,
+                galleryConfig = _props.galleryConfig,
+                menuToolbarOption = _props.menuToolbarOption,
+                readOnly = _props.readOnly,
+                renderPlaceholder = _props.renderPlaceholder,
+                rest = _objectWithoutProperties(_props, ['value', 'onChange', 'serviceConfig', 'galleryConfig', 'menuToolbarOption', 'readOnly', 'renderPlaceholder']);
+
+            var isFull = this.state.isFull;
+
+
+            return readOnly ? React.createElement(CannerEditor, {
+                value: value,
+                onChange: function onChange(arg) {
+                    return arg;
+                },
+                readOnly: readOnly
+            }) : React.createElement(
+                Container,
+                _extends({ isFull: isFull }, rest),
+                isFull ? React.createElement(
+                    FixedToolbar,
+                    null,
+                    React.createElement(_menuToolbar2.default, {
+                        isFull: true,
+                        value: value,
+                        serviceConfig: serviceConfig,
+                        galleryConfig: galleryConfig,
+                        menuToolbarOption: menuToolbarOption,
+                        onChange: onChange,
+                        goFull: this.goFull
+                    })
+                ) : React.createElement(_menuToolbar2.default, {
+                    serviceConfig: serviceConfig,
+                    galleryConfig: galleryConfig,
+                    menuToolbarOption: menuToolbarOption,
+                    value: value,
+                    onChange: onChange,
+                    goFull: this.goFull
+                }),
+                React.createElement(
+                    EditorContainer,
+                    { isFull: isFull },
+                    React.createElement(CannerEditor, _extends({
+                        renderPlaceholder: renderPlaceholder,
+                        value: value
+                    }, rest, {
+                        onChange: onChange,
+                        readOnly: readOnly
+                    }))
+                )
+            );
+        }
+    }]);
+
+    return EditorComponent;
 }(React.Component);
 
 exports.default = EditorComponent;
 var CannerEditor = (_dec = (0, _slateToolbar2.default)(toolbarOptions), _dec2 = (0, _slateSidebar2.default)(sidebarOptions), _dec(_class2 = _dec2(_class2 = function (_React$Component2) {
-  _inherits(CannerEditor, _React$Component2);
+    _inherits(CannerEditor, _React$Component2);
 
-  function CannerEditor() {
-    _classCallCheck(this, CannerEditor);
+    function CannerEditor() {
+        _classCallCheck(this, CannerEditor);
 
-    return _possibleConstructorReturn(this, (CannerEditor.__proto__ || Object.getPrototypeOf(CannerEditor)).apply(this, arguments));
-  }
-
-  _createClass(CannerEditor, [{
-    key: "shouldComponentUpdate",
-    value: function shouldComponentUpdate(nextProps) {
-      if (this.props.value === nextProps.value) return false;
-      return true;
+        return _possibleConstructorReturn(this, (CannerEditor.__proto__ || Object.getPrototypeOf(CannerEditor)).apply(this, arguments));
     }
-  }, {
-    key: "render",
-    value: function render() {
-      var _props2 = this.props,
-          value = _props2.value,
-          onChange = _props2.onChange,
-          readOnly = _props2.readOnly,
-          renderPlaceholder = _props2.renderPlaceholder;
 
-      return React.createElement(_slateReact.Editor, {
-        className: "markdown-body",
-        value: value,
-        renderPlaceholder: renderPlaceholder,
-        readOnly: readOnly,
-        onChange: onChange,
-        plugins: plugins
-      });
-    }
-  }]);
+    _createClass(CannerEditor, [{
+        key: 'shouldComponentUpdate',
+        value: function shouldComponentUpdate(nextProps) {
+            if (this.props.value === nextProps.value) return false;
+            return true;
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var _props2 = this.props,
+                value = _props2.value,
+                onChange = _props2.onChange,
+                readOnly = _props2.readOnly,
+                rest = _objectWithoutProperties(_props2, ['value', 'onChange', 'readOnly']);
 
-  return CannerEditor;
+            return React.createElement(_slateReact.Editor, _extends({
+                className: 'markdown-body',
+                value: value
+            }, rest, {
+                readOnly: readOnly,
+                onChange: onChange,
+                plugins: plugins
+            }));
+        }
+    }]);
+
+    return CannerEditor;
 }(React.Component)) || _class2) || _class2);
 
 /***/ }),
